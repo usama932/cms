f520f99041c033fb2dfea34610c5223d3ff62ebe
Usama Qayyum <78843627+usama932@users.noreply.github.com>
2023-08-14 19:26:29 -0700

    push

--- app/Http/Middleware/GenerateMenus.php
+++ app/Http/Middleware/GenerateMenus.php
@@ -41,7 +41,18 @@ class GenerateMenus
                 ->link->attr([
                     'class' => 'nav-link',
                 ]);
-
+                $menu->add('<i class="nav-icon fas fa-bell"></i> Notifications', [
+                    'route' => 'backend.notifications.index',
+                    'class' => 'nav-item',
+                ])
+                    ->data([
+                        'order' => 99,
+                        'activematches' => 'admin/notifications*',
+                        'permission' => [],
+                    ])
+                    ->link->attr([
+                        'class' => 'nav-link',
+                    ]);
             // Separator: Access Management
             $menu->add('Management', [
                 'class' => 'nav-title',
@@ -66,18 +77,18 @@ class GenerateMenus
                 ]);
 
             // Backup
-            $menu->add('<i class="nav-icon fas fa-archive"></i> Backups', [
-                'route' => 'backend.backups.index',
-                'class' => 'nav-item',
-            ])
-                ->data([
-                    'order' => 103,
-                    'activematches' => 'admin/backups*',
-                    'permission' => ['view_backups'],
-                ])
-                ->link->attr([
-                    'class' => 'nav-link',
-                ]);
+            // $menu->add('<i class="nav-icon fas fa-archive"></i> Backups', [
+            //     'route' => 'backend.backups.index',
+            //     'class' => 'nav-item',
+            // ])
+            //     ->data([
+            //         'order' => 103,
+            //         'activematches' => 'admin/backups*',
+            //         'permission' => ['view_backups'],
+            //     ])
+            //     ->link->attr([
+            //         'class' => 'nav-link',
+            //     ]);
 
             // Access Control Dropdown
             $accessControl = $menu->add('<i class="nav-icon fa-solid fa-user-gear"></i> Access Control', [
@@ -126,46 +137,46 @@ class GenerateMenus
 
             // Log Viewer
             // Log Viewer Dropdown
-            $accessControl = $menu->add('<i class="nav-icon fa-solid fa-list-check"></i> Log Viewer', [
-                'class' => 'nav-group',
-            ])
-                ->data([
-                    'order' => 107,
-                    'activematches' => [
-                        'log-viewer*',
-                    ],
-                    'permission' => ['view_logs'],
-                ]);
-            $accessControl->link->attr([
-                'class' => 'nav-link nav-group-toggle',
-                'href' => '#',
-            ]);
+            // $accessControl = $menu->add('<i class="nav-icon fa-solid fa-list-check"></i> Log Viewer', [
+            //     'class' => 'nav-group',
+            // ])
+            //     ->data([
+            //         'order' => 107,
+            //         'activematches' => [
+            //             'log-viewer*',
+            //         ],
+            //         'permission' => ['view_logs'],
+            //     ]);
+            // $accessControl->link->attr([
+            //     'class' => 'nav-link nav-group-toggle',
+            //     'href' => '#',
+            // ]);
 
             // Submenu: Log Viewer Dashboard
-            $accessControl->add('<i class="nav-icon fa-solid fa-list"></i> Dashboard', [
-                'route' => 'log-viewer::dashboard',
-                'class' => 'nav-item',
-            ])
-                ->data([
-                    'order' => 108,
-                    'activematches' => 'admin/log-viewer',
-                ])
-                ->link->attr([
-                    'class' => 'nav-link',
-                ]);
+            // $accessControl->add('<i class="nav-icon fa-solid fa-list"></i> Dashboard', [
+            //     'route' => 'log-viewer::dashboard',
+            //     'class' => 'nav-item',
+            // ])
+            //     ->data([
+            //         'order' => 108,
+            //         'activematches' => 'admin/log-viewer',
+            //     ])
+            //     ->link->attr([
+            //         'class' => 'nav-link',
+            //     ]);
 
             // Submenu: Log Viewer Logs by Days
-            $accessControl->add('<i class="nav-icon fa-solid fa-list-ol"></i> Logs by Days', [
-                'route' => 'log-viewer::logs.list',
-                'class' => 'nav-item',
-            ])
-                ->data([
-                    'order' => 109,
-                    'activematches' => 'admin/log-viewer/logs*',
-                ])
-                ->link->attr([
-                    'class' => 'nav-link',
-                ]);
+            // $accessControl->add('<i class="nav-icon fa-solid fa-list-ol"></i> Logs by Days', [
+            //     'route' => 'log-viewer::logs.list',
+            //     'class' => 'nav-item',
+            // ])
+            //     ->data([
+            //         'order' => 109,
+            //         'activematches' => 'admin/log-viewer/logs*',
+            //     ])
+            //     ->link->attr([
+            //         'class' => 'nav-link',
+            //     ]);
 
             // Access Permission Check
             $menu->filter(function ($item) {
